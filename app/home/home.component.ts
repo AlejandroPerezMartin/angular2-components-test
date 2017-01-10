beb32f72987f8bd6d7173e46f9e09fedb2a7f433
Alejandro Perez <aleperez92@gmail.com>
2017-01-10 11:55:44 +0100

    Managing components

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -11,13 +11,25 @@ import { WidgetThree } from '../widgets/widget-three.component'
             <p>widget two content</p>
         </widget-two>
         <button (click)="addComponent()">Add component</button>
+        <button (click)="moveComponent()">Move component</button>
+        <button (click)="moveComponent()">Remove component</button>
         <div #container></div>
+        <hr>
+        <button (click)="createTemplate()">Create template</button>
+        <template #template let-description="description">
+            <h2>My {{description}} template</h2>
+            <button>My {{description}} button</button>
+        </template>
     `
 })
 
 export class HomeComponent {
 
     @ViewChild('container', {read: ViewContainerRef}) container;
+
+    @ViewChild('template') template;
+
+    widgetRef;
 
     constructor(
         private simpleService: SimpleService,
@@ -30,16 +42,31 @@ export class HomeComponent {
         this.container.createComponent(widgetFactory);
         this.container.createComponent(widgetFactory);
 
-        const widgetRef = this.container.createComponent(widgetFactory, 2); // number indicates order
-        widgetRef.instance.message = "I'm third";
+        this.widgetRef = this.container.createComponent(widgetFactory, 2); // number indicates order
+        this.widgetRef.instance.message = "I'm third";
     }
 
     addComponent() {
         const widgetFactory = this.resolver.resolveComponentFactory(WidgetThree)
         this.container.createComponent(widgetFactory);
 
-        const widgetRef = this.container.createComponent(widgetFactory, 3); // number indicates order
-        widgetRef.instance.message = "I'm fourth";
+        this.widgetRef = this.container.createComponent(widgetFactory, 3); // number indicates order
+        this.widgetRef.instance.message = "I'm fourth";
+    }
+
+    moveComponent() {
+        const randomIndex = Math.floor(Math.random() * this.container.length);
+        this.container.move(this.widgetRef.hostView, randomIndex);
+    }
+
+    removeComponent() {
+        this.widgetRef.detach(2); // argument is the index
+    }
+
+    createTemplate() {
+        this.container.createEmbeddedView(this.template, {
+            description: 'amazing'
+        });
     }
 
 }
--- dist/home/home.component.js
+++ dist/home/home.component.js
@@ -21,14 +21,26 @@ var HomeComponent = (function () {
         this.container.createComponent(widgetFactory);
         this.container.createComponent(widgetFactory);
         this.container.createComponent(widgetFactory);
-        var widgetRef = this.container.createComponent(widgetFactory, 2); // number indicates order
-        widgetRef.instance.message = "I'm third";
+        this.widgetRef = this.container.createComponent(widgetFactory, 2); // number indicates order
+        this.widgetRef.instance.message = "I'm third";
     };
     HomeComponent.prototype.addComponent = function () {
         var widgetFactory = this.resolver.resolveComponentFactory(widget_three_component_1.WidgetThree);
         this.container.createComponent(widgetFactory);
-        var widgetRef = this.container.createComponent(widgetFactory, 3); // number indicates order
-        widgetRef.instance.message = "I'm fourth";
+        this.widgetRef = this.container.createComponent(widgetFactory, 3); // number indicates order
+        this.widgetRef.instance.message = "I'm fourth";
+    };
+    HomeComponent.prototype.moveComponent = function () {
+        var randomIndex = Math.floor(Math.random() * this.container.length);
+        this.container.move(this.widgetRef.hostView, randomIndex);
+    };
+    HomeComponent.prototype.removeComponent = function () {
+        this.widgetRef.detach(2); // argument is the index
+    };
+    HomeComponent.prototype.createTemplate = function () {
+        this.container.createEmbeddedView(this.template, {
+            description: 'amazing'
+        });
     };
     return HomeComponent;
 }());
@@ -36,10 +48,14 @@ __decorate([
     core_1.ViewChild('container', { read: core_1.ViewContainerRef }),
     __metadata("design:type", Object)
 ], HomeComponent.prototype, "container", void 0);
+__decorate([
+    core_1.ViewChild('template'),
+    __metadata("design:type", Object)
+], HomeComponent.prototype, "template", void 0);
 HomeComponent = __decorate([
     core_1.Component({
         selector: 'home',
-        template: "\n        <div>I'm a home component</div>\n        <widget-one [message]=\"simpleService.message\"></widget-one>\n        <widget-two>\n            <p>widget two content</p>\n        </widget-two>\n        <button (click)=\"addComponent()\">Add component</button>\n        <div #container></div>\n    "
+        template: "\n        <div>I'm a home component</div>\n        <widget-one [message]=\"simpleService.message\"></widget-one>\n        <widget-two>\n            <p>widget two content</p>\n        </widget-two>\n        <button (click)=\"addComponent()\">Add component</button>\n        <button (click)=\"moveComponent()\">Move component</button>\n        <button (click)=\"moveComponent()\">Remove component</button>\n        <div #container></div>\n        <hr>\n        <button (click)=\"createTemplate()\">Create template</button>\n        <template #template let-description=\"description\">\n            <h2>My {{description}} template</h2>\n            <button>My {{description}} button</button>\n        </template>\n    "
     }),
     __metadata("design:paramtypes", [services_module_1.SimpleService,
         core_1.ComponentFactoryResolver])
